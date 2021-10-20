1440996b79ebfd7cd4cf9c1d43ef2255c93b5f18
MarkBebrov <92856826+MarkBebrov@users.noreply.github.com>
2021-10-20 13:59:02 +0300

    Add files via upload

---
+++ SOROKLETKAKPODNARKOZOM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+
+            m = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write('*');
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
