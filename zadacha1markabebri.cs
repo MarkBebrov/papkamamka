5a1b617d190d4d93a73f5bfb7c6e0612440d21ca
MarkBebrov <92856826+MarkBebrov@users.noreply.github.com>
2021-10-20 13:07:24 +0300

    Add files via upload

---
+++ zadacha1markabebri.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c;
+            c = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                c = c + i;
+
+            }
+            Console.WriteLine(c);
+        }
+    }
+}
