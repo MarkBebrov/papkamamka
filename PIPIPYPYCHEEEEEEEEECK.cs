16921f582b39ebb7ebcfd6c9753629847fa2ca2b
MarkBebrov <92856826+MarkBebrov@users.noreply.github.com>
2021-10-20 14:12:04 +0300

    Add files via upload

---
+++ PIPIPYPYCHEEEEEEEEECK.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+
+
+            for (int i = 2; i <= 6; i++)
+            {
+                for (int j = 2; j <= 9; j++)
+                {
+                    c = i * j;
+                    
+
+                    Console.Write(i);
+
+                    Console.Write("*");
+
+                    Console.Write(j);
+
+                    Console.Write("=");
+
+                    Console.Write(i * j);
+                    Console.WriteLine();
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+    }
+
