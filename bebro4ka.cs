04f01effec1c529245c7b37ee44ae0af93f47486
MarkBebrov <92856826+MarkBebrov@users.noreply.github.com>
2021-10-20 13:29:19 +0300

    Add files via upload

---
+++ bebro4ka.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+    class Program2
+    {
+        static void Main11(string[] args)
+        {
+            int n, c, k;
+            c = 1;
+            k = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                k = k + 1;
+                c = c * k;
+
+
+
+            } while (k < n);
+            
+            Console.WriteLine(c);
+        Console.ReadKey();
+        }
+    }
+
