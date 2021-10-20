78969bc29045119266be3c55dbcf1be118bdba77
MarkBebrov <92856826+MarkBebrov@users.noreply.github.com>
2021-10-20 13:45:47 +0300

    Add files via upload

---
+++ ayayayayybilinegra.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c, kakkakakakak;
+            c = 0;
+            kakkakakakak = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i <= n; i = i + 2)
+            {
+                c = c + i;
+                kakkakakakak = kakkakakakak + 1;
+            }
+
+
+
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+    }
+
